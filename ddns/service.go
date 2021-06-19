3b0c0f533eec7a4bf87a87f9e1b63d7a7a908d2b
Jurgen Schatz <Jurgen>
2021-06-19 16:47:07 +0200

    ddns functionality completed

--- ddns/service.go
+++ ddns/service.go
@@ -2,9 +2,12 @@ package ddns
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"net/http"
 	"os/exec"
+	"strings"
+	"time"
 )
 
 type Service interface {
@@ -18,7 +21,12 @@ type service struct {
 
 func NewDDNSService() Service {
 	s := &service{}
-	s.GetPublicIP()
+	go func() {
+		for now := range time.Tick(time.Minute) {
+			log.Println("Confirming Public IP validity at: " + now.Format("03:04:05 02/01/2006"))
+			s.GetPublicIP()
+		}
+	}()
 	return s
 }
 
@@ -34,15 +42,18 @@ func (s *service) GetPublicIP() {
 		log.Println(err)
 	}
 
-	if s.CurrentPublicIP != out.String() {
-		log.Println("Updating current public IP to: " + out.String())
-		s.CurrentPublicIP = (out.String())
+	Ip := strings.TrimSuffix(out.String(), "\n")
+
+	if s.CurrentPublicIP != Ip {
+		log.Println("Updating current public IP to: " + Ip)
+		s.CurrentPublicIP = Ip
 		s.UpdateNoIP()
 
 	}
 }
 func (s *service) UpdateNoIP() {
-	_, err := http.Get("http://jurgen.schatz@gmail.com:Gr33nfus3@dynupdate.no-ip.com/nic/update?hostname=schatzcorp.ddns.net&myip=" + s.CurrentPublicIP)
+
+	_, err := http.Get(fmt.Sprintf("http://jurgen.schatz@gmail.com:Gr33nfus3@dynupdate.no-ip.com/nic/update?hostname=schatzcorp.ddns.net&myip=%v", s.CurrentPublicIP))
 	if err != nil {
 		log.Println(err)
 	}
